f95d5e8c350f1eb0c982bc5797b08f901663ea38
Ugur GELISKEN <ugurgelisken@gmail.com>
2021-08-02 01:18:27 +0300

    brands llnk

--- components/brands/index.js
+++ components/brands/index.js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 import styles from "../../styles/Brands.module.css";
 
@@ -12,14 +13,18 @@ const Brands = () => {
   return (
     <div className={styles.container}>
       <div className={styles.brand}>
-        <img className={styles.brandLogo} src="assets/logo-modiverse.jpg" />
-        <img className={styles.brandName1} src="assets/modiverse.png" />
-        <p className={styles.brandText}>{t.commons.brand2Text}</p>
+        <a href="/modiverse">
+          <img className={styles.brandLogo} src="assets/logo-modiverse.jpg" />
+          <img className={styles.brandName1} src="assets/modiverse.png" />
+          <p className={styles.brandText}>{t.commons.brand2Text}</p>
+        </a>
       </div>
       <div className={styles.brand}>
-        <img className={styles.brandLogo} src="assets/logo-iot-ignite.jpg" />
-        <img className={styles.brandName2} src="assets/iot-ignite.png" />
-        <p className={styles.brandText}>{t.commons.brand2Text}</p>
+        <a href="/iot-ignite">
+          <img className={styles.brandLogo} src="assets/logo-iot-ignite.jpg" />
+          <img className={styles.brandName2} src="assets/iot-ignite.png" />
+          <p className={styles.brandText}>{t.commons.brand2Text}</p>
+        </a>
       </div>
     </div>
   );
