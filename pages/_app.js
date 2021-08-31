945f6fe8a3ff35b96df4d390142b13f16a96750b
Ugur Gelisken <ugur.gelisken@ardictech.com>
2021-08-31 20:15:08 +0300

    .

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import "bootstrap/dist/css/bootstrap.css";
 
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import { motion } from "framer-motion";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
 
@@ -22,25 +21,11 @@ function App({ Component, pageProps }) {
     router.events.on("routeChangeComplete", () => {
       NProgress.done();
     });
-
-    const handleRouteChange = (url) => {
-      if (typeof window !== "undefined") {
-        window.gtag("config", process.env.GA_TRACKING_ID, {
-          page_location: url,
-        });
-      }
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
   }, [router.events]);
 
   return (
     <Layout>
-     
-        <Component {...pageProps} />
-     
+      <Component {...pageProps} />
     </Layout>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 export default class CustomDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
 
   render() {
     return (
